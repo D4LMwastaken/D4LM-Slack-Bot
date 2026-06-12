d108c44a16ccf704bff6dfcb17a1eb17d8660fb9
D4LM <148903901+D4LMwastaken@users.noreply.github.com>
2026-06-11 20:03:36 -0400

    Feat: Implemented /D4LM-Ask!

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/revrost/go-openrouter"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/socketmode"
+	"google.golang.org/genai"
 )
 
 func main() {
@@ -20,13 +20,12 @@ func main() {
 
 	appToken := os.Getenv("SLACK_APP_TOKEN")
 	botToken := os.Getenv("SLACK_BOT_TOKEN")
-	openRouterToken := os.Getenv("HACK_CLUB_API_KEY")
 
-	openRouterConfig := openrouter.DefaultConfig(openRouterToken)
+	geminiClient, err := genai.NewClient(context.Background(), &genai.ClientConfig{})
 
-	openRouterConfig.BaseURL = "https://ai.hackclub.com/proxy/v1"
-
-	openRouterClient := openrouter.NewClientWithConfig(*openRouterConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Following the example for the API...
 	api := slack.New(botToken, slack.OptionAppLevelToken(appToken))
@@ -58,32 +57,25 @@ func main() {
 					}
 					client.Ack(*evt.Request, payload)
 				case "/d4lm-ask":
-					// There is a 3-second timeout for a reason hopefully
-					client.Ack(*evt.Request, nil)
+					response, err := geminiClient.Models.GenerateContent(
+						context.Background(),
+						"gemini-3.1-flash-lite",
+						genai.Text("Very quickly (less than 3 seconds), answer this question: "+cmd.Text),
+						nil,
+					)
 
-					go func(cmd slack.SlashCommand) {
-						resp, err := openRouterClient.CreateChatCompletion(
-							context.Background(),
-							openrouter.ChatCompletionRequest{
-								Model: "~google/gemini-flash-latest", // Use a valid model string
-								Messages: []openrouter.ChatCompletionMessage{
-									openrouter.UserMessage(cmd.Text),
-								},
-							})
+					if err != nil {
+						log.Fatal(err)
+					}
 
-						if err != nil {
-							fmt.Println("LLM Error:", err)
-							api.PostEphemeral(cmd.ChannelID, cmd.UserID, slack.MsgOptionText("Sorry, I had trouble thinking: "+err.Error(), false))
-							return
-						}
-
-						msgText := "Response: \n" + resp.Choices[0].Message.Content.Text
-						api.PostMessage(cmd.ChannelID, slack.MsgOptionText(msgText, false))
-					}(cmd)
+					payload := &slack.TextBlockObject{
+						Type: slack.MarkdownType,
+						Text: "Please note that this command is quickly processed: " + response.Text(),
+					}
+					client.Ack(*evt.Request, payload)
 				}
 			}
 		}
 	}()
-
 	client.Run()
 }
